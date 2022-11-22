8eb9dfc496843321a49ea6216cc9f551ce9761c8
devon creamer <devoncreamer@gmail.com>
2022-11-21 20:03:28 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'social.media.app',
+  appName: 'social-media-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
